e3ddbb67e761702c65c736fbf095bf567145a528
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-09-18 11:25:56 -0600

    added script to download dataset

---
+++ dl-data.sh
@@ -0,0 +1,3 @@
+curl http://205.174.165.80/CICDataset/DoHBrw-2020/Dataset/BenignDoH-NonDoH-CSVs.zip -O
+curl http://205.174.165.80/CICDataset/DoHBrw-2020/Dataset/MaliciousDoH-CSVs.zip -O 
+curl http://205.174.165.80/CICDataset/DoHBrw-2020/Dataset/Total-CSVs.zip -O
